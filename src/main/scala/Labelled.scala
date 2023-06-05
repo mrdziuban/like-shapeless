d6e99e71479e8c9ddf8bce00165897bbe492020c
Matt Dziuban <mrdziuban@gmail.com>
2023-06-05 15:38:21 -0400

    Remove unnecessary code.

--- src/main/scala/Labelled.scala
+++ src/main/scala/Labelled.scala
@@ -1,10 +1,3 @@
 package likeShapeless
 
 opaque type ->>[K, +V] = V
-object ->> {
-  implicit def convertToV[K, V](kv: K ->> V): V = kv
-}
-
-extension[K <: Singleton](k: K) {
-  inline final def ->>[V](v: V): K ->> V = v
-}
